d373b8f36b47379c772d4d4584553ff0c536c264
Jorge Mondaca Fabrega <mondaca.jorge.l2@gmail.com>
2022-09-26 17:59:36 -0300

    Minor updates - 4

--- admin/section/file_manager.php
+++ admin/section/file_manager.php
@@ -2,7 +2,24 @@
 require('../template/header.php');
 ?>
 
-Files
+<div class="col-md-5">
+ <form method="POST">
+  <div class = "form-group">
+  <label for="document">Document Numer/Date</label>
+  <input type="text" class="form-control" name ="document" id="document" placeholder="Document Number/Date">
+  </div>
+  <div class="form-group">
+  <label for="detail">Details</label>
+  <textarea class="form-control" id="detail" name="detail" placeholder="Details"></textarea>
+  </div><br/>
+  <button type="submit" class="btn btn-primary">Sign In</button>
+ </form>
+ 
+ 
+</div>
+<div class="col-md-7">
+ Data
+</div>
 
 <?php
 require('../template/footer.php');
--- admin/section/hiring_manager.php
+++ admin/section/hiring_manager.php
@@ -2,7 +2,13 @@
 require('../template/header.php');
 ?>
 
-Hirings
+<div class="col-md-5">
+    Form
+</div>
+<div class="col-md-7">
+    Data
+</div>
+
 
 <?php
 require('../template/footer.php');
